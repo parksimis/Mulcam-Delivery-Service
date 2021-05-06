14f07732c6bf2bbcedb34367c9c547ed58bfc125
ParkSeonIk <parksimis@gmail.com>
2021-05-06 11:15:07 +0900

    Refactor : userfunc refactoring 유저정의 함수 리팩토링 - 기능 및 주석 추가

--- django/userfunc.py
+++ django/userfunc.py
@@ -35,6 +35,12 @@ def select_main_table():
 
 
 def select_all(table=None):
+    '''
+    각 업종별 테이블의 데이터를 모두 조회하는 method
+    :param table: 원하는 테이블
+        테이블 목록 = [bunsik, cafe, chicken, etc, fastfood, hansik, ilsik, jimtang, jokbal, jungsik, yasik]
+    :return: 원하는 테이블 쿼리 결과
+    '''
     conn = sqlite3.connect('./db.sqlite3')
     cur = conn.cursor()
 
@@ -53,6 +59,11 @@ def select_all(table=None):
 
 
 def add_datetime_column(df):
+    '''
+    테이블의 결과의 시간 컬럼을 연-월-일로 컬럼을 붙여주는 메소드
+    :param df: 테이블 조회 결과(DataFrame)
+    :return: 테이블에 연-월-일 컬럼이 추가된 결과
+    '''
     df['시간'] = pd.to_datetime(df['시간'])
 
     df['year'] = df['시간'].dt.year
@@ -63,6 +74,13 @@ def add_datetime_column(df):
 
 
 def preprocessing_df(df, formula=None):
+    '''
+    조회 결과를 받아 원하는 형식으로 전처리하는 메소드
+    :param df: 전처리할 dataFrame
+    :param formula: 1) 일자별 - 일자별로 groupby해 평균 계산(일자별 평균 주문건수)
+                    2) 시간대별 - 시간대별로 groupby해 평균 계산(시간대별 주문건수)
+    :return: 전처리된 DataFrame
+    '''
     if formula == '일자별':
 
         tmp_df = df.loc[(df['year'] == max(df['year'])) & (df['month'] == max(df['month']))].reset_index(drop=True)
@@ -93,6 +111,12 @@ def preprocessing_df(df, formula=None):
 
 
 def cutting_area(df, cut_column):
+    '''
+    서울과 경기도로 데이터프레임을 나누는 메소드
+    :param df: 처리할 dataFrame
+    :param cut_column: 서울 or 경기도 선택
+    :return: cut_column으로 나눠진 dataframe
+    '''
     tmp = df.loc[df['광역시도명'] == cut_column].reset_index(drop=True)
     tmp.drop('광역시도명', axis=1, inplace=True)
 
@@ -100,6 +124,11 @@ def cutting_area(df, cut_column):
 
 
 def divide_df(df):
+    '''
+    시간과 주문건수로 나누어주는 메소드
+    :param df: 나눌 데이터프레임
+    :return: 시간과 주문건수 list
+    '''
     table1 = df['시간'].values
     table2 = df['주문건수'].values
 
@@ -107,6 +136,14 @@ def divide_df(df):
 
 
 def execute_all(table, formula):
+    '''
+    위의 모든 과정을 한꺼번에 실행해서 결과를 반환
+    :param table: 원하는 테이블
+            테이블 목록 = [bunsik, cafe, chicken, etc, fastfood, hansik, ilsik, jimtang, jokbal, jungsik, yasik]
+    :param formula: 1) 일자별 - 일자별로 groupby해 평균 계산(일자별 평균 주문건수)
+                    2) 시간대별 - 시간대별로 groupby해 평균 계산(시간대별 주문건수)
+    :return:
+    '''
     table = select_all(table)
 
     add_column_tbl = add_datetime_column(table)
@@ -131,8 +168,12 @@ def execute_all(table, formula):
 
 
 def main_line_data():
-    model_list = ['chicken', 'ilsik', 'bunsik', 'yasik', 'jokbal', 'jungsik', 'jimtang', 'cafe', 'fastfood', 'hansik',
-                  'etc']
+    '''
+    메인 화면의 전체 배달 주문건수에 대한 데이터를 얻기위한 메소드
+    :return: 모든 테이블에 대한 일별 합계 데이터
+    '''
+    model_list = ['chicken', 'ilsik', 'bunsik', 'yasik', 'jokbal',
+                  'jungsik', 'jimtang', 'cafe', 'fastfood', 'hansik', 'etc']
 
     result = pd.DataFrame(columns=['광역시도명', '시간', '주문건수', '업종명'])
 
@@ -162,8 +203,13 @@ def main_line_data():
 
 
 def main_pie_data():
-    model_list = ['chicken', 'ilsik', 'bunsik', 'yasik', 'jokbal', 'jungsik', 'jimtang', 'cafe', 'fastfood', 'hansik',
-                  'etc']
+    '''
+    메인 화면의 파이 그래프에 들어갈 데이터를 전처리해주는 메소드
+    파이그래프 - 월별 주문비율
+    :return: 각 테이블 별 월별 주문 비율 리스트
+    '''
+    model_list = ['chicken', 'ilsik', 'bunsik', 'yasik', 'jokbal',
+                  'jungsik', 'jimtang', 'cafe', 'fastfood', 'hansik', 'etc']
 
     result = pd.DataFrame(columns=['광역시도명', '시간', '주문건수', '업종명'])
 
